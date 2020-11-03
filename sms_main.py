336b739307b0080afdf43e8f2685f47fd7ab9b5a
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2020-11-03 09:57:18 +0530

    Main code file

--- sms_main.py
+++ sms_main.py
@@ -4,10 +4,10 @@ from tkinter import *
 from tkinter.messagebox import showerror,showinfo
 
 def send_sms(number, message):
-    url = "https://www.fast2sms.com/dev/bulk"
+    url = " " #put your URL from the website
     params = {
-        'authorization' : 'JLX7bm0UsAS2EqWRGxFCh48wcPanBp9gIriHuVyTdkt3NDzjQ6GiKm6NWwtHQ8MZF1ckyb9fsqSJTedX',
-        'sender_id' : 'SMSIND',
+        'authorization' : '',  # put the Authorization API here
+        'sender_id' : ' ', # put the sender_id here
         'message' : message,
         'language' : 'english',
         'route' : 'p',
@@ -26,25 +26,3 @@ def btn_click():
         showinfo("Success!!","Message sent successfully")
     else:
         showerror("Error while sending","Something went wrong. Try again!")
-
-
-
-# creating a GUI
-root = Tk()
-root.title ("Messenger version 2.0.0")
-root.geometry("400x625")
-font = ('Times new roman', 18, 'bold')
-
-textNumber = Entry(root, font = font)
-textNumber.pack(fill = X, pady = 20)
-
-font1 = ('times new roman', 14)
-textMsg = Text(root, font = font1)
-textMsg.pack(fill = X)
-
-sendBtn = Button(root, text = "SEND MESSAGE", command = btn_click)
-sendBtn.pack()
-
-
-
-root.mainloop()
